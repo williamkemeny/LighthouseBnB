264956a9e089a688eef42098162671879b9ac015
William Kemeny <williamkemeny@gmail.com>
2023-07-06 16:44:18 -0400

    added seeds

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,25 @@
+-- Seeds created by me (not used in DB, used seeds created by LHL)
+  
+  -- users ENTITY 
+  INSERT INTO users(name, email, password) 
+  VALUES ('Eva Stanley', 'sebastianguerra@ymail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'), 
+  ('Louisa Meyer', 'jacksonrose@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'), 
+  ('Dominic Parks', 'victoriablackwell@outlook.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+  -- properties ENTITY
+  INSERT INTO properties(owner_id, title, description,thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active) 
+  VALUES (1, 'Speed lamp', 'description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 930.61, 6,4, 8, 'Canada', '536 Namsub Highway', 'Sotboske', 'Quebec', 28142, true),
+  (2, 'Blank corner', 'description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 85234, 6,6,7, 'Canada', '651 Nami Road', 'Bohbatev', 'Alberta', 83680, true),
+  (3, 'Habit mix', 'description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 46058, 0,5,6, 'Canada', '1650 Hejto Center', 'Genwezuj', 'Newfoundland And Labrador', 44583, true);
+
+  -- reservations ENTITY
+  INSERT INTO reservations (start_date, end_date, property_id, guest_id)
+  VALUES ('2018-09-11', '2018-09-26', 2, 3),
+  ('2019-01-04', '2019-02-01', 2, 2),
+  ('2021-10-01', '2021-10-14', 1, 1);
+
+  -- property_reviews ENTITY
+  INSERT INTO property_reviews(guest_id,property_id, reservation_id, rating, message)
+  VALUES (2,2,3,3,'message'),
+  (1,3,1,4,'message'),
+  (3,1,2,4, 'message');
